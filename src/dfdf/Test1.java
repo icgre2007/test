d268410fff128ebb525ba9b1c242cc26b190def0
icgre2007 <icgre@sohu.com>
2015-12-07 18:14:44 +0800

    11

--- src/dfdf/Test1.java
+++ src/dfdf/Test1.java
@@ -3,7 +3,7 @@ package dfdf;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("icgreen");
+		System.out.println("icgreen1");
 		// TODO Auto-generated method stub
 
 	}
